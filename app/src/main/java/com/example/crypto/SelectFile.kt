e2ff36d8054f377703119bf67e00f323db31e8d4
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-03-25 02:26:54 +0530

    file picker

--- app/src/main/java/com/example/crypto/SelectFile.kt
+++ app/src/main/java/com/example/crypto/SelectFile.kt
@@ -1,11 +1,47 @@
 package com.example.crypto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import com.google.android.material.button.MaterialButton
 
 class SelectFile : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+
+    companion object {
+        val IMAGE_REQUEST_CODE = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_file)
+
+        val btnSelectFile = findViewById<MaterialButton>(R.id.btnbrowse)
+        val btnEncrypt = findViewById<MaterialButton>(R.id.btnencryption)
+        imageView = findViewById(R.id.imageView)
+
+        btnSelectFile.setOnClickListener {
+            pickImageGallery()
+
+        }
+
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageView.setImageURI(data?.data)
+        }
     }
 }
+
