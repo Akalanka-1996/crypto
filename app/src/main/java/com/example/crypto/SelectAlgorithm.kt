6faad391ee856c53abe28bd15b48bce4014c52ac
Akalanka-1996 <kasunakalanka4@gmail.com>
2022-04-03 00:57:10 +0530

    select algorithm updated

--- app/src/main/java/com/example/crypto/SelectAlgorithm.kt
+++ app/src/main/java/com/example/crypto/SelectAlgorithm.kt
@@ -1,21 +1,78 @@
 package com.example.crypto
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class SelectAlgorithm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_algorithm)
 
+        val toggleButtonGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleButtonGroup)
+
+
+        toggleButtonGroup.addOnButtonCheckedListener { toggleButtonGroup, checkedId, isChecked ->
+            if (isChecked) {
+                when (checkedId) {
+                    R.id.btntext -> {
+                        showToast("Text selected")
+                        selectTextAlgorithm()
+                    }
+                    R.id.btnfile -> {
+                        showToast("File selected.")
+                        selectFileAlgorithm()
+
+                    }
+                }
+            } else {
+                if (toggleButtonGroup.checkedButtonId == View.NO_ID) {
+                    showToast("Nothing Selected")
+                }
+            }
+        }
+
+
+    }
+
+    private fun selectTextAlgorithm() {
         val btnBlowfish = findViewById<MaterialButton>(R.id.btnblowfish)
         val btnAES = findViewById<MaterialButton>(R.id.btnaes)
         val btnDES = findViewById<MaterialButton>(R.id.btndes)
+        //  blowfish
 
-        // blowfish
+        btnBlowfish.setOnClickListener {
+            Toast.makeText(this, "Blowfish algorithm selected", Toast.LENGTH_SHORT).show()
+            var Intent = Intent(this, EncryptText::class.java)
+            startActivity(Intent)
+        }
+
+        // AES
+
+        btnAES.setOnClickListener {
+            Toast.makeText(this, "AES algorithm selected", Toast.LENGTH_SHORT).show()
+            var Intent = Intent(this, EncryptText::class.java)
+            startActivity(Intent)
+        }
+
+        // DES
+
+        btnDES.setOnClickListener {
+            Toast.makeText(this, "DES algorithm selected", Toast.LENGTH_SHORT).show()
+            var Intent = Intent(this, EncryptText::class.java)
+            startActivity(Intent)
+        }
+    }
+
+    private fun selectFileAlgorithm() {
+        val btnBlowfish = findViewById<MaterialButton>(R.id.btnblowfish)
+        val btnAES = findViewById<MaterialButton>(R.id.btnaes)
+        val btnDES = findViewById<MaterialButton>(R.id.btndes)
+        //  blowfish
 
         btnBlowfish.setOnClickListener {
             Toast.makeText(this, "Blowfish algorithm selected", Toast.LENGTH_SHORT).show()
@@ -38,7 +95,9 @@ class SelectAlgorithm : AppCompatActivity() {
             var Intent = Intent(this, SelectFile::class.java)
             startActivity(Intent)
         }
+    }
 
-
+    private fun showToast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 }
